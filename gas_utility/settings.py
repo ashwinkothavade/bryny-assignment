461ac93b22cbaa583a20c2ebed997467fe67b889
ashwinkothavade <ashwinkothavade@gmail.com>
2025-03-28 08:40:55 +0530

    enhanced css

--- gas_utility/settings.py
+++ gas_utility/settings.py
@@ -119,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'service_requests/static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files
 MEDIA_URL = '/media/'
